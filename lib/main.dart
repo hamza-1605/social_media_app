5b94f52ac99b624314431bedb4f8791e9b8d2383
Hamza Sajid~ <hamzasajid1165@gmail.com>
2026-02-12 16:50:29 +0500

    Linked Firebase project

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:social_media_app/app.dart';
 
-void main() async{
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
-  w
+
   runApp(const MyApp());
 }
